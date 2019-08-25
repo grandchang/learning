afc13dbc2f8288a049f2f0c5f1e83af3a3d7cf63
grandchang <grand.chang@gmail.com>
2019-08-25 23:11:09 +0800

    add file into repositor-a

---
+++ hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+print('Hello, World!')
+print ('The quick brown fox','jumps over','the lazy dog')
+# print (300)
+# print(100+200)
+
+print('What is your name?')
+name=input()
+print('hello,', name)
